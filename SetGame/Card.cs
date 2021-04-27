23b07ef825e08931e67ed484f343c47fe62f7cd9
Brad Tyler <brad.tyler@intimetec.com>
2021-04-27 10:25:58 -0600

    Add project files.

---
+++ SetGame/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SetGame
+{
+    public struct Card
+    {
+        public Enum Shape;
+        public Enum color;
+        public Enum shade;
+        public Enum number;
+
+    }
+}
